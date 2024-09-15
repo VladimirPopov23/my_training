45daab650a0017f9460f54729b44d0ae87cfe576
VladimirPopov23 <volodyaklever@gmail.com>
2024-09-15 19:15:15 +0300

    module_1_5.py
    15.09.2024 Неизменяемые и изменяемые объекты. Кортежи и списки.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-# module_1_4.py
-# 12/09/2024 Практическое задание по теме "Организация программ и методы строк".
-my_string=input("Введите фразу: ")
-print(len(my_string))
-print(my_string .upper())
-print(my_string .lower())
-print(my_string .replace(" ", ""))
-print(my_string [0])
-print(my_string [-1])
+# module_1_5.py
+# 15.09.2024 Неизменяемые и изменяемые объекты. Кортежи и списки
+immutable_var=([9], 1, True, "string", 1.5)
+print(immutable_var)
+immutable_var[0][0]=5
+print("Immutable var:", immutable_var)
+# immutable_var[1][1]=5
+# Изменить неизменные элементы в кортеже нельзя, т.к объект не поддерживает данную функцию.
+mutable_list=[1, 2, "a", "b"]
+mutable_list[0]="one"
+mutable_list[1]="two"
+mutable_list[2]="A"
+mutable_list[3]="B"
+mutable_list.append("Modified")
+print("Mutable list:", mutable_list)
