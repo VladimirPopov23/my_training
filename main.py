cfb798c7b4c31905d938673d23c46a9cc8c29f3d
VladimirPopov23 <volodyaklever@gmail.com>
2024-09-16 18:37:07 +0300

    Practice_module_1.py
    16.09.2024 Дополнительное практическое задание по модулю "Базовые структуры данных".

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-# module_1_6.py
-# 15.09.2024 Словари и множества.
-my_dict = {"Vladimir" : 1990, "Nastya" : 1992, "Bonaparte" : 1812}
-print("Dict:", my_dict)
-print("Existing value:", my_dict["Vladimir"])
-print("Not existing value:", my_dict.get("Napoleon"))
-my_dict.update({"Gogol" : 1809, "Tolstoy" : 1828})
-print("Deleted value:", my_dict.pop("Bonaparte"))
-print("Modified dictionary:", my_dict)
-my_set={"W", "W", "W", "Leningrad", "SPB", ".ru"}
-print("Set:", my_set)
-my_set.add("Vincent van Gogh")
-my_set.add(7)
-my_set.remove(".ru")
-print("Modified set:", my_set)
+# Practice_module_1.py
+# 16.09.2024 Дополнительное практическое задание по модулю "Базовые структуры данных".
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = sorted(students)
+students_ = {students[0]:(sum(grades[0])/len(grades[0])), students[1]:(sum(grades[1])/len(grades[1])), students[2]:(sum(grades[2])/len(grades[2])), students[3]:(sum(grades[3])/len(grades[3])), students[4]:(sum(grades[4])/len(grades[4]))}
+print(students_)
