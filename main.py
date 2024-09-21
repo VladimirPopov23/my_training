6420d52cfdc6a9ca2205d1a2089f8042247d077a
VladimirPopov23 <volodyaklever@gmail.com>
2024-09-21 20:23:37 +0300

    module_2_3
    Нули ничто, отрицание недопустимо!

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-# module_2_2.py
-# 18.09.2024 Условная конструкция. Операторы if, elif, else.
-first = int(input("Введите первое число: "))
-second = int(input("Введите второе число: "))
-third = int(input("Введите третье число: "))
-if first == second and second == third and first == third:
-    print(3)
-elif first == second or second == third or first == third:
-    print(2)
-else:
-    print(0)
+# module_2_3.py
+# 21.09.2024 Нули ничто, отрицание недопустимо!
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+my_list.remove(0)
+while a < len(my_list):
+    if my_list[a] < 0:
+        break
+    print(my_list[a])
+    a = a + 1
