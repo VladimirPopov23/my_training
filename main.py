d3e8d81ac136d1a003b84127c8af7027dddbfedc
VladimirPopov23 <volodyaklever@gmail.com>
2024-09-22 22:24:09 +0300

    module_2_3.py
    # 21.09.2024 Нули ничто, отрицание недопустимо!
    # 22.09.2024 Без использования ф-ции .remove()

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # module_2_3.py
 # 21.09.2024 Нули ничто, отрицание недопустимо!
+# 22.09.2024 Без использования ф-ции .remove(0)
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
-my_list.remove(0)
 while a < len(my_list):
-    if my_list[a] < 0:
+    if my_list[a] == 0:
+        a = a + 1
+        continue
+    elif my_list[a] < 0:
         break
     print(my_list[a])
     a = a + 1
