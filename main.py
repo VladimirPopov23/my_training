6817b1a7d7696f3835185758bb9e5ace373c64a3
VladimirPopov23 <volodyaklever@gmail.com>
2024-09-23 21:32:52 +0300

    module_2_5.py
    23.09.2024 Задача "Матрица воплоти"

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-# module_2_4.py
-# 22.09.2024 Задача "Всё не так уж просто"
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes = []
-not_primes = []
-for n in numbers:
-    is_prime = True
-    if n == 1:
-        continue
-    elif n > 1:
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                is_prime = False
-                break
-    else:
-        is_prime = False
-    if is_prime:
-        primes.append(n)
-    else:
-        not_primes.append(n)
-print("Primes: ", primes)
-print("Not Primes: ", not_primes)
+# module_2_5.py
+# 23.09.2024 Задача "Матрица воплоти"
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
