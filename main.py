72b6a902258bd586f0322f701344faeb4c45d173
VladimirPopov23 <volodyaklever@gmail.com>
2024-09-29 20:16:45 +0300

    module_3_3.py
    29.09.2024 Задача "Распаковка"

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-# module_3_2.py
-# 29.09.2024 Задача "Рассылка писем" (устранение замечаний)
+# module_3_3.py
+# 29.09.2024 Задача "Распаковка"
 
-def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    x = (".com", ".ru", ".net")
-    index_r = recipient.find("@")
-    index_s = sender.find("@")
-    if int(index_r) > 0 and int(index_s) > 0:
-        index = True
-    else:
-        index = False
-    for n in recipient and sender:
-        if recipient.endswith(x) and sender.endswith(x):
-            n = True
-        else:
-            n = False
-    if sender == "university.help@gmail.com" and index == True and n == True:
-        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
-    if sender != "university.help@gmail.com" and recipient != sender and index == True and n == True:
-        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
-    if index == False or n == False:
-        print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
-    if recipient == sender:
-        print("Нельзя отправить письмо самому себе!")
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
 
+# 1. Функция с параметрами по умолчанию:
+print_params()
+print_params(b = 25)
+print_params(c = [1, 2, 3])
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+# 2.Распаковка параметров:
+values_list = [9, 'Max', False]
+values_dict = {'a':5, 'b':'list', 'c':False}
+print_params(*values_list)
+print_params(**values_dict)
+
+# 3.Распаковка + отдельные параметры:
+values_list_2 = [3.14, 'mail']
+print_params(*values_list_2, 42)
+
