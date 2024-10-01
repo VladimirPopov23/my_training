7d61921df1420689b40127cf916e19b673104acf
VladimirPopov23 <volodyaklever@gmail.com>
2024-10-01 21:16:32 +0300

    module_3_4.py
    01.10.2024 Задача "Однокоренные"

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-# module_3_5.py
-# 30.09.2024 "Рекурсивное умножение цифр"
+# module_3_4.py
+# 01.10.2024 Задача "Однокоренные"
 
-def get_multiplied_digits(number):
-    str_number = str(number)
-    first = int(str_number[0])
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return first
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in range(len(other_words)):
+        m = str(other_words[i].lower()).count(str(root_word.lower()))
+        n = str(root_word.lower()).count(str(other_words[i].lower()))
+        if m != 0:
+            same_words.append(other_words[i])
+        if n != 0:
+            same_words.append(other_words[i])
+    return same_words
 
-
-result = get_multiplied_digits(40203)
-print(result)
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
